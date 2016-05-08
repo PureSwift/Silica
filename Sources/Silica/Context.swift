0f1dbe3a56cfe74b7ef0cfc3d18484f35ac89350
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-05-08 14:51:21 -0500

    created Xcode Project

---
+++ Sources/Silica/Context.swift
@@ -0,0 +1,23 @@
+//
+//  Context.swift
+//  Silica
+//
+//  Created by Alsey Coleman Miller on 5/8/16.
+//  Copyright © 2016 PureSwift. All rights reserved.
+//
+
+import Cairo
+
+public final class Context {
+    
+    // MARK: - Properties
+    
+    public let surface: Cairo.Surface
+    
+    // MARK: - Initialization
+    
+    public init(surface: Cairo.Surface, size: CGSize) {
+        
+        
+    }
+}
---
+++ Sources/Silica/Path.swift
@@ -0,0 +1,8 @@
+//
+//  Path.swift
+//  Silica
+//
+//  Created by Alsey Coleman Miller on 5/8/16.
+//  Copyright © 2016 PureSwift. All rights reserved.
+//
+
