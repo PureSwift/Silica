221d36048f46513f7dc4c0290ac858f0b48c6a78
Alsey Coleman Miller <alseycmiller@gmail.com>
2016-05-12 02:13:39 -0500

    added CGRect extensions

--- Sources/CoreGraphics/CGRect.swift
+++ Sources/CoreGraphics/CGRect.swift
@@ -9,3 +9,36 @@
 import Silica
 
 public typealias CGRect = Silica.Rect
+
+public extension CGRect {
+    
+    public var minX: CGFloat {
+        
+        return (size.width < 0) ? origin.x + size.width : origin.x
+    }
+    
+    public var midX: CGFloat {
+        
+        return origin.x + (size.width / 2.0)
+    }
+    
+    public var maxX: CGFloat {
+        
+        return (size.width < 0) ? origin.x : origin.x + size.width
+    }
+    
+    public var minY: CGFloat {
+        
+        return (size.height < 0) ? origin.y + size.height : origin.y
+    }
+    
+    public var midY: CGFloat {
+        
+        return origin.y + (size.height / 2.0)
+    }
+    
+    public var maxY: CGFloat {
+        
+        return (size.height < 0) ? origin.y : origin.y + size.height
+    }
+}
