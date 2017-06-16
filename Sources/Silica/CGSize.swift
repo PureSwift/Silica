3cac69edbc8483bd602fa6064d6697a47c860acf
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-06-16 09:35:17 -0500

    Added NSGeometry extensions

---
+++ Sources/Silica/CGSize.swift
@@ -0,0 +1,14 @@
+//
+//  CGSize.swift
+//  Silica
+//
+//  Created by Alsey Coleman Miller on 6/16/17.
+//
+
+import struct Foundation.CGFloat
+import struct Foundation.CGSize
+
+public extension CGSize {
+    
+    public static var zero: CGSize { return CGSize() }
+}
