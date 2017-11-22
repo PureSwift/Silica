c9a28052f06dc022609bab110c23e3df7a2e907d
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-11-22 16:53:19 -0500

    [SR-6451] Fixed runtime Segmentation Fault bug
    https://bugs.swift.org/browse/SR-6451

--- Sources/Silica/CGAffineTransform.swift
+++ Sources/Silica/CGAffineTransform.swift
@@ -26,11 +26,6 @@ public struct CGAffineTransform {
     
     // MARK: - Initialization
     
-    public init(a: CGFloat, b: CGFloat, c: CGFloat, d: CGFloat, tx: CGFloat, ty: CGFloat) {
-        
-        self.init(a: a, b: b, c: c, d: d, tx: tx, ty: ty)
-    }
-    
     public static let identity = CGAffineTransform(a: 1, b: 0, c: 0, d: 1, tx: 0, ty: 0)
 }
 
