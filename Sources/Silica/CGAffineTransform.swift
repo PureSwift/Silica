8927a7c7b797a5e187174284e3fa8f30aa25503e
Alsey Coleman Miller <alseycmiller@gmail.com>
2017-11-22 18:16:18 -0500

    Fixed CGAffineTransform

--- Sources/Silica/CGAffineTransform.swift
+++ Sources/Silica/CGAffineTransform.swift
@@ -26,6 +26,16 @@ public struct CGAffineTransform {
     
     // MARK: - Initialization
     
+    public init(a: CGFloat, b: CGFloat, c: CGFloat, d: CGFloat, tx: CGFloat, ty: CGFloat) {
+        
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.tx = tx
+        self.ty = ty
+    }
+    
     public static let identity = CGAffineTransform(a: 1, b: 0, c: 0, d: 1, tx: 0, ty: 0)
 }
 
